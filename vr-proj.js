39e5ce7b32a2ef1e3a99422581891d6a0b510de1
James McCarthy <jmccarth@uwaterloo.ca>
2019-08-13 15:08:56 -0400

    Added more controls

--- vr-proj.js
+++ vr-proj.js
@@ -1,3 +1,38 @@
+// Rotate globe with keyboard events
+window.addEventListener("keydown", function(e) {
+  let globe = this.document.querySelector("#globeModel");
+  let rotation_factor = 0.1;
+  if (e.keyCode === 74) { //j
+    globe.object3D.rotation.x += rotation_factor;
+  }
+  else if (e.keyCode == 75) { //k
+    globe.object3D.rotation.y += rotation_factor;
+  }
+  else if (e.keyCode == 76) { //l
+    globe.object3D.rotation.z += rotation_factor;
+  }
+  else if (e.keyCode === 73) { //j
+    globe.object3D.rotation.x -= rotation_factor;
+  }
+  else if (e.keyCode == 79) { //k
+    globe.object3D.rotation.y -= rotation_factor;
+  }
+  else if (e.keyCode == 80) { //l
+    globe.object3D.rotation.z -= rotation_factor;
+  }
+  else if (e.keyCode == 82){
+    globe.object3D.rotation.x = 0;
+    globe.object3D.rotation.y = 0;
+    globe.object3D.rotation.z = 0;
+  }
+  else if (e.keyCode == 67){
+    this.document.querySelector("#camera").object3D.position.set(0, 3.5, 10)
+  }
+
+
+});
+
+// Click on the cylinder to create a cylindrical projection
 AFRAME.registerComponent('cylinder-listener',{
   init: function() {
     var button = document.querySelector("#cylButton");
@@ -14,6 +49,7 @@ AFRAME.registerComponent('cylinder-listener',{
   }
 });
 
+// Click on the cone to create a conic projection
 AFRAME.registerComponent('cone-listener',{
   init: function() {
     var button = document.querySelector("#coneButton");
@@ -30,6 +66,7 @@ AFRAME.registerComponent('cone-listener',{
   }
 });
 
+// Click on the plane to create a flat projection
 AFRAME.registerComponent('plane-listener',{
   init: function() {
     var button = document.querySelector("#planeButton");
