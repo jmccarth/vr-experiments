479bc2acfa016d49e7f2f180cf50ec519d250d51
James McCarthy <jmccarth@uwaterloo.ca>
2019-09-18 15:28:38 -0400

    projection machine with sensible help

--- vr-proj.js
+++ vr-proj.js
@@ -20,65 +20,45 @@ window.addEventListener("keydown", function(e) {
   else if (e.keyCode == 80) { //l
     globe.object3D.rotation.z -= rotation_factor;
   }
-  else if (e.keyCode == 82){
+  else if (e.keyCode == 82){ //r
     globe.object3D.rotation.x = 0;
     globe.object3D.rotation.y = 0;
     globe.object3D.rotation.z = 0;
   }
-  else if (e.keyCode == 67){
+  else if (e.keyCode == 67){ //c
     this.document.querySelector("#camera").object3D.position.set(0, 3.5, 10)
   }
-
-
-});
-
-// Click on the cylinder to create a cylindrical projection
-AFRAME.registerComponent('cylinder-listener',{
-  init: function() {
-    var button = document.querySelector("#cylButton");
-    button.addEventListener('click', function(evt){
-      console.log("clicked!");
-      let cyl_vis = document.querySelector("#outerCylinder").getAttribute("visible");
-      let cone_vis = document.querySelector("#outerCone").getAttribute("visible");
-      let plane_vis = document.querySelector("#outerPlane").getAttribute("visible");
-      
-      document.querySelector("#outerCylinder").setAttribute("visible",true);
-      document.querySelector("#outerCone").setAttribute("visible",false);
-      document.querySelector("#outerPlane").setAttribute("visible",false);
-    });
+  else if (e.keyCode == 49){ //1
+    let cyl_vis = document.querySelector("#outerCylinder").getAttribute("visible");
+    let cone_vis = document.querySelector("#outerCone").getAttribute("visible");
+    let plane_vis = document.querySelector("#outerPlane").getAttribute("visible");
+    
+    document.querySelector("#outerCylinder").setAttribute("visible",false);
+    document.querySelector("#outerCone").setAttribute("visible",false);
+    document.querySelector("#outerPlane").setAttribute("visible",true);
   }
-});
-
-// Click on the cone to create a conic projection
-AFRAME.registerComponent('cone-listener',{
-  init: function() {
-    var button = document.querySelector("#coneButton");
-    button.addEventListener('click', function(evt){
-      console.log("clicked!");
-      let cyl_vis = document.querySelector("#outerCylinder").getAttribute("visible");
-      let cone_vis = document.querySelector("#outerCone").getAttribute("visible");
-      let plane_vis = document.querySelector("#outerPlane").getAttribute("visible");
-      
-      document.querySelector("#outerCylinder").setAttribute("visible",false);
-      document.querySelector("#outerCone").setAttribute("visible",true);
-      document.querySelector("#outerPlane").setAttribute("visible",false);
-    });
+  else if (e.keyCode == 50){ //2
+    let cyl_vis = document.querySelector("#outerCylinder").getAttribute("visible");
+    let cone_vis = document.querySelector("#outerCone").getAttribute("visible");
+    let plane_vis = document.querySelector("#outerPlane").getAttribute("visible");
+    
+    document.querySelector("#outerCylinder").setAttribute("visible",false);
+    document.querySelector("#outerCone").setAttribute("visible",true);
+    document.querySelector("#outerPlane").setAttribute("visible",false);
   }
-});
-
-// Click on the plane to create a flat projection
-AFRAME.registerComponent('plane-listener',{
-  init: function() {
-    var button = document.querySelector("#planeButton");
-    button.addEventListener('click', function(evt){
-      console.log("clicked!");
-      let cyl_vis = document.querySelector("#outerCylinder").getAttribute("visible");
-      let cone_vis = document.querySelector("#outerCone").getAttribute("visible");
-      let plane_vis = document.querySelector("#outerPlane").getAttribute("visible");
-      
-      document.querySelector("#outerCylinder").setAttribute("visible",false);
-      document.querySelector("#outerCone").setAttribute("visible",false);
-      document.querySelector("#outerPlane").setAttribute("visible",true);
-    });
+  else if (e.keyCode == 51){ //3
+    let cyl_vis = document.querySelector("#outerCylinder").getAttribute("visible");
+    let cone_vis = document.querySelector("#outerCone").getAttribute("visible");
+    let plane_vis = document.querySelector("#outerPlane").getAttribute("visible");
+    
+    document.querySelector("#outerCylinder").setAttribute("visible",true);
+    document.querySelector("#outerCone").setAttribute("visible",false);
+    document.querySelector("#outerPlane").setAttribute("visible",false);
   }
+  else if (e.keyCode == 72){ //h
+    let help_vis = document.querySelector("#help").getAttribute("visible");
+    document.querySelector("#help").setAttribute("visible",!help_vis);
+  }
+
+
 });
